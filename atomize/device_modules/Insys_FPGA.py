252b3d17fb08a42b4988c9ac02ab434d81767673
Anatoly <anatoly.melnikov@tomo.nsc.ru>
2026-04-15 13:26:29 +0700

    2026-04-15; insys_fpga updated

--- atomize/device_modules/Insys_FPGA.py
+++ atomize/device_modules/Insys_FPGA.py
@@ -4398,8 +4398,9 @@ class Insys_FPGA:
                 except UnboundLocalError:
                     pass
 
-                new_ans = self.process_and_merge_rect_awg(answer, target_channel=128, gap_threshold=55)
-                return new_ans
+
+                #new_ans = self.process_and_merge_rect_awg(answer, target_channel=128, gap_threshold=75)
+                return answer
 
         elif self.test_flag == 'test':
             # according to 0 element (channel number)
@@ -4426,48 +4427,53 @@ class Insys_FPGA:
                 except UnboundLocalError:
                     pass
 
-                new_ans = self.process_and_merge_rect_awg(answer, target_channel=128, gap_threshold=55)
+                #new_ans = self.process_and_merge_rect_awg(answer, target_channel=128, gap_threshold=75)
 
-                return new_ans
+                return answer
 
+    #mod
     def process_and_merge_rect_awg(self, data_list, target_channel=128, gap_threshold=70):
-        if not data_list:
-            return np.array([])
+        if data_list is None or len(data_list) == 0:
+            return []
         
-        all_data = np.vstack(data_list)
+        all_data = np.array(data_list)
         
         target_mask = all_data[:, 0] == target_channel
         target_rows = all_data[target_mask]
         other_rows = all_data[~target_mask]
         
         if len(target_rows) == 0:
-            return all_data
+            return np.split(all_data, np.where(np.diff(all_data[:, 0]))[0] + 1)
 
         target_rows = target_rows[target_rows[:, 1].argsort()]
         
         merged_target = []
-        if len(target_rows) > 0:
-            current = target_rows[0].copy()
+        current = target_rows[0].copy()
+        
+        for i in range(1, len(target_rows)):
+            next_row = target_rows[i]
             
-            for i in range(1, len(target_rows)):
-                next_row = target_rows[i]
-                
-                gap = next_row[1] - current[2]
-                
-                if gap < gap_threshold:
-                    current[2] = max(current[2], next_row[2])
-                else:
-                    merged_target.append(current)
-                    current = next_row.copy()
+            gap = next_row[1] - current[2]
             
-            merged_target.append(current)
-
+            if gap < gap_threshold:
+                current[2] = max(current[2], next_row[2])
+            else:
+                merged_target.append(current.copy())
+                current = next_row.copy()
+        
+        merged_target.append(current.copy())
         merged_target_arr = np.array(merged_target)
-        final_result = np.vstack([other_rows, merged_target_arr])
-        final_result = final_result[final_result[:, 0].argsort()]
-        res_list = np.split(final_result, np.where(np.diff(final_result[:, 0]))[0] + 1)
 
-        return res_list
+        if len(other_rows) == 0:
+            final_result = merged_target_arr
+        else:
+            final_result = np.vstack([other_rows, merged_target_arr])
+
+        final_result = final_result[final_result[:, 0].argsort()]
+        #diff_indices = np.where(np.diff(final_result[:, 0].astype(float)))[0] + 1
+        #res_list = np.split(final_result, diff_indices)
+
+        return final_result
 
     def extending_rect_awg_pulser(self, np_array):
         """
@@ -4612,6 +4618,7 @@ class Insys_FPGA:
             no_duplicate_array = np.unique(np_array, axis = 0)
             return no_duplicate_array
 
+    #mod
     def preparing_to_bit_pulse_pulser(self, np_array):
         """
         For pulses at each channel we check whether there is overlapping pulses using 
@@ -4701,12 +4708,10 @@ class Insys_FPGA:
                             cor_pulses_amp_final = cor_pulses_amp
                         elif cor_pulses_amp[0][0] == 0:
                             # nothing to concatenate
-                            amp_on_pulses = self.convert_to_bit_pulse_amp_lna_pulser(prob_pulses_amp, \
-                                self.channel_dict_pulser['AMP_ON'])
+                            amp_on_pulses = self.convert_to_bit_pulse_amp_lna_pulser(prob_pulses_amp, self.channel_dict_pulser['AMP_ON'])
                             cor_pulses_amp_final = self.instruction_pulse_short_lna_amp_pulser(amp_on_pulses)
                         else:
-                            amp_on_pulses = self.convert_to_bit_pulse_amp_lna_pulser(prob_pulses_amp, \
-                                self.channel_dict_pulser['AMP_ON'])
+                            amp_on_pulses = self.convert_to_bit_pulse_amp_lna_pulser(prob_pulses_amp, self.channel_dict_pulser['AMP_ON'])
                             try:
                                 #cor_pulses_amp_final = cor_pulses_amp, self.instruction_pulse_short_lna_amp_pulser(amp_on_pulses)
                                 cor_pulses_amp_final = np.concatenate((cor_pulses_amp, self.instruction_pulse_short_lna_amp_pulser(amp_on_pulses)), axis = 0)
@@ -4739,10 +4744,11 @@ class Insys_FPGA:
                 # combine all pulses
                 #np.concatenate( (self.convertion_to_numpy_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final), axis = None)
                 try:
-                    #return np.row_stack( (self.convertion_to_numpy_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final))
-                    # self.extending_rect_awg_pulser( self.pulse_array_pulser ) is for extendind RECT_AWG pulses
-                    # see self.extending_rect_awg_pulser()
-                    return np.row_stack( (self.extending_rect_awg_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final))
+                    #return np.row_stack( (self.extending_rect_awg_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final))
+
+                    raw = np.row_stack( (self.extending_rect_awg_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final))
+                    answer = self.process_and_merge_rect_awg(raw, target_channel=128, gap_threshold=75)
+                    return answer
 
                 # when we do not MW pulses at all
                 except UnboundLocalError:
@@ -4851,7 +4857,12 @@ class Insys_FPGA:
                 # combine all pulses
                 #np.concatenate( (self.convertion_to_numpy_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final), axis = None) 
                 try:
-                    return np.row_stack( (self.extending_rect_awg_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final))
+                    raw = np.row_stack( (self.extending_rect_awg_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final))
+                    answer = self.process_and_merge_rect_awg(raw, target_channel=128, gap_threshold=75)
+                    return answer
+                    
+                    #return np.row_stack( (self.extending_rect_awg_pulser( self.pulse_array_pulser ), cor_pulses_amp_final, cor_pulses_lna_final))
+
                 except UnboundLocalError:
                     return self.extending_rect_awg_pulser( self.pulse_array_pulser )
 
